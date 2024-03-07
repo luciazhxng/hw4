7d275654d9694013b8e10470941904ace43e4a48
Lucia Zhang <lrzhang@usc.edu>
2024-03-07 00:19:16 +0000

    finish equal-paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -12,7 +12,39 @@ using namespace std;
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+  if(root == nullptr) {
+    return true;
+  }
+  if(root->left != nullptr && root->right != nullptr) {
+    if(getHeights(root->left) != getHeights(root->right)) {
+      return false;
+    }
+    return equalPaths(root->right) && equalPaths(root->left);
+  }
+  if(root->left != nullptr) {
+    root = root->left; 
+    if(getHeights(root->left) != getHeights(root->right)) {
+      return false;
+    }
+    return equalPaths(root->right) && equalPaths(root->left);
+  }
+  if(root->right != nullptr) {
+    root = root->right; 
+    if(getHeights(root->left) != getHeights(root->right)) {
+      return false;
+    }
+    return equalPaths(root->right) && equalPaths(root->left);
+  }
+  return true;
 }
-
+int getHeights(Node * root) {
+	if(root == nullptr) {
+		return 0; 
+	}
+	int left = getHeights(root->left);
+	int right = getHeights(root->right);
+	if(left > right) {
+		return 1 + left;
+	}
+	return 1 + right;
+}
